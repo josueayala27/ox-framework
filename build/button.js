da69244fbc3796e5f6a00aadef3f20675c1c0e07
josue.ayala <ayalavalencia11@gmail.com>
2020-10-02 13:27:28 -0600

    Create button.js

---
+++ build/button.js
@@ -0,0 +1,16 @@
+let buttons = document.querySelectorAll('button')
+buttons.forEach(btn => {
+    btn.addEventListener('click', function(e) {
+        let x = e.clientX - e.target.offsetLeft;
+        let y = e.clientY - e.target.offsetTop;
+        let ripples = document.createElement('div');
+        ripples.style.left = x + 'px';
+        ripples.style.top = y + 'px';
+        ripples.className += "ox-ripple-container";
+        ripples.innerHTML = '<div class="ox-ripple"></div>';
+        this.appendChild(ripples)
+        setTimeout(() => {
+            ripples.remove()
+        }, 1000)
+    })
+})
